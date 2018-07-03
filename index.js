c9e6bf673f03d6ddcc939afc89d5968f82a818ba
Jonathan Nguyen <jnnj12@yahoo.com>
2018-07-03 20:22:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ function happyHolidays (hh) {
 }
 
 
+<<<<<<< HEAD
 function happyHolidayTo (holiday, name) {
   return (`Happy ${holiday}, ${name}!`)
 }
@@ -16,4 +17,15 @@ function happyHolidaysTo (name) {
 
 function holidayCountdown (holiday, days) {
   return (`It's ${days} days until ${holiday}!`)
+=======
+function happyHol
+
+function happyHolidaysTo (holiday, name) {
+  return (`Happy ${holiday}, ${name}!`)
+}
+
+
+function holidayCountdown (holidays, days){
+  return (`It's ${days} until ${holidays}`)
+>>>>>>> c6623af7ab8a0751c638e0efbec84777c5fccd6d
 }
